264e683309609fafb60f42135b20c3a924e1de36
Anthony Kroh <ajkroh@gmail.com>
2018-05-31 09:18:54 -0400

    adds rgb sleep to boards with underglow

--- keyboards/bigseries/config.h
+++ keyboards/bigseries/config.h
@@ -54,6 +54,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN D3
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 5
+#define RGBLIGHT_SLEEP
 #endif
 
 #endif
