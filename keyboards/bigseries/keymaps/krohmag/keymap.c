ab8d540814f755da2ad00375d430b8d8ec98a4d7
Anthony Kroh <ajkroh@gmail.com>
2018-07-10 14:10:37 -0400

    changes LED color for big switch

--- keyboards/bigseries/keymaps/krohmag/keymap.c
+++ keyboards/bigseries/keymaps/krohmag/keymap.c
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-KEYMAP(
+LAYOUT(
     KC_A),
 
 
@@ -37,7 +37,7 @@ void matrix_init_user(void) {
       dprintf("Initializing in matrix_scan_user");
       rgblight_enable();
       rgblight_mode(1);
-      rgblight_sethsv(25,255,255);
+      rgblight_sethsv(180, 255, 153);
       initialized = 1;
     }
 }
